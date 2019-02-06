c80c9a426f2f9387ed512dc8d4d2dbeb25c64017
Enrique Peñaloza <epenaloza@novopayment.com>
2019-02-06 09:31:29 -0400

    Crea metodos y vistas para editar y eliminar

--- app/Controllers/Http/TasksController.js
+++ app/Controllers/Http/TasksController.js
@@ -1,4 +1,5 @@
 'use strict'
+const Logger = use('Logger')
 const Task = use('App/Models/Task')
 
 class TasksController {
@@ -26,11 +27,34 @@ class TasksController {
 
   async details({params, view}) {
     const task = await Task.find(params.id)
-    console.log(task)
+    Logger.info('task------ ', task)
 
     return view.render('tasks.details', {
       task
     })
+  }
+
+  async edit({params, view}) {
+    const task = await Task.find(params.id)
+
+    return view.render('tasks.edit', {task})
+  }
+
+  async update({params, request, response}) {
+    const task = await Task.find(params.id)
+    task.title = request.input('title')
+    task.body = request.input('body')
+
+    await task.save()
+
+    return response.redirect('/tasks')
+  }
+
+  async destroy({params, response}) {
+    const task = await Task.find(params.id)
+    await task.delete()
+
+    return response.redirect('/tasks')
   }
 }
 
